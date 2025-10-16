b32849f298e771cf5b857a26e6c3e66dc1f2afea
tomioka-tsukasa <tsukasa.creator@gmail.com>
2025-10-16 15:12:54 +0900

    Revert "Merge pull request #1 from tomioka-tsukasa/aura-nest"
    This reverts commit eb76546ab6e64a2572462c539b0b2dbfd1401ffb, reversing
    changes made to 7ee358846121f957dc62a281ae057f616d0b3644.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,48 +1,9 @@
-import { ImgTitleDesc, Button, ImgCaption } from '../../components/ui'
-import * as styles from './Home.css'
-
 const Home = () => {
-  return (
-    <section id={'section-sample'} className={styles.container}>
-      <h1 className={styles.heading}>UI Components Demo</h1>
-
-      <div className={styles.section}>
-        <h2 className={styles.subheading}>ImgTitleDesc コンポーネント</h2>
-        <div className={styles.grid}>
-          <ImgTitleDesc
-            imageSrc='https://api.builder.io/api/v1/image/assets/TEMP/dd14d85ba95cc5de8fa230d4b16a51cb3a606122?width=460'
-            title='タイトル'
-            description='説明文'
-            pattern='default'
-            icon='none'
-          />
-          <ImgTitleDesc
-            imageSrc='https://api.builder.io/api/v1/image/assets/TEMP/3645bb201388bc8f8b1662cc0f8ea7d656c22872?width=460'
-            title='タイトル'
-            description='説明文'
-            pattern='white_bg'
-            icon='arrow'
-          />
-        </div>
-      </div>
-
-      <div className={styles.section}>
-        <h2 className={styles.subheading}>Button コンポーネント</h2>
-        <div className={styles.buttonGroup}>
-          <Button label='ラベル' size='default' />
-          <Button label='ラベル' size='large' />
-        </div>
-      </div>
-
-      <div className={styles.section}>
-        <h2 className={styles.subheading}>ImgCaption コンポーネント</h2>
-        <ImgCaption
-          imageSrc='https://api.builder.io/api/v1/image/assets/TEMP/0305028a7ede20c89f55953d5d7fe9c89e3c6188?width=1516'
-          caption='キャプション'
-        />
-      </div>
+  return <>
+    <section id={'section-sample'}>
+      Home Page
     </section>
-  )
+  </>
 }
 
 export default Home
